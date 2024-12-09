1abebdc052823b2ce0951fed6675ab3ac342bc38
Mr-Kesh <123339974+Mr-Kesh@users.noreply.github.com>
2024-12-08 17:45:48 -0700

    Add files via upload

---
+++ ClassPortal.js
@@ -0,0 +1,14 @@
+function closeModal() {
+    document.getElementById('click-toggle').checked = false;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerIcon = document.getElementById("hamburger-icon");
+    const navMenu = document.getElementById("nav-menu");
+
+    hamburgerIcon.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+        hamburgerIcon.classList.toggle("active");
+    });
+});
+
